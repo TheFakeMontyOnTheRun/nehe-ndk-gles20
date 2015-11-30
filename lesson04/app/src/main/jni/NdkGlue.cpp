5ba1a2656864214308a0e59d06357c55135cd42b
= <danielmonteiro@id.uff.br>
2015-11-30 21:44:34 -0200

    Initial state for Lesson 4
    And for initial, I mean already working as intended. Kind of.
    I still need to remember precisely what was the effect to be
    achieved. Also, I want to refactor and optmize some things.
    
    Please notice that the ticker thread was done from Java rather
    than C++, so the Framework has greater control over the
    rendering code.

---
+++ lesson04/app/src/main/jni/NdkGlue.cpp
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// OpenGL ES 2.0 code
+
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+
+#include "GLES2Lesson.h"
+#include "NdkGlue.h"
+
+static std::string gVertexShader;
+static std::string gFragmentShader;
+
+static int android_read(void *cookie, char *buf, int size) {
+    return AAsset_read((AAsset *) cookie, buf, size);
+}
+
+static int android_write(void *cookie, const char *buf, int size) {
+    return EACCES; // can't provide write access to the apk
+}
+
+static fpos_t android_seek(void *cookie, fpos_t offset, int whence) {
+    return AAsset_seek((AAsset *) cookie, offset, whence);
+}
+
+static int android_close(void *cookie) {
+    AAsset_close((AAsset *) cookie);
+    return 0;
+}
+
+
+FILE *android_fopen(const char *fname, const char *mode, AAssetManager *assetManager) {
+    if (mode[0] == 'w') return NULL;
+
+    AAsset *asset = AAssetManager_open(assetManager, fname, 0);
+    if (!asset) return NULL;
+
+    return funopen(asset, android_read, android_write, android_seek, android_close);
+}
+
+
+std::string readShaderToString(FILE *fileDescriptor) {
+    const unsigned N = 1024;
+    std::string total;
+    while (true) {
+        char buffer[N];
+        size_t read = fread((void *) &buffer[0], 1, N, fileDescriptor);
+        if (read) {
+            for (int c = 0; c < read; ++c) {
+                total.push_back(buffer[c]);
+            }
+        }
+        if (read < N) {
+            break;
+        }
+    }
+
+    return total;
+}
+
+void loadShaders(JNIEnv *env, jobject &obj) {
+    AAssetManager *asset_manager = AAssetManager_fromJava(env, obj);
+    FILE *fd;
+    fd = android_fopen("vertex.glsl", "r", asset_manager);
+    gVertexShader = readShaderToString(fd);
+    fclose(fd);
+    fd = android_fopen("fragment.glsl", "r", asset_manager);
+    gFragmentShader = readShaderToString(fd);
+    fclose(fd);
+}
+
+bool setupGraphics(int w, int h) {
+    return GLES2Lesson::init(w, h, gVertexShader.c_str(), gFragmentShader.c_str());
+}
+
+void renderFrame() {
+    GLES2Lesson::render();
+}
+
+void shutdown() {
+    GLES2Lesson::shutdown();
+}
+
+void tick() {
+    GLES2Lesson::tick();
+}
+
+extern "C" {
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_onCreate(JNIEnv *env, void *reserved,
+                                                                    jobject assetManager);
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_onDestroy(JNIEnv *env, jobject obj);
+
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_init(JNIEnv *env, jobject obj,
+                                                                jint width, jint height);
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_step(JNIEnv *env, jobject obj);
+
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_tick(JNIEnv *env, jobject obj);
+};
+
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_onCreate(JNIEnv *env, void *reserved,
+                                                                    jobject assetManager) {
+    loadShaders(env, assetManager);
+}
+
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_init(JNIEnv *env, jobject obj,
+                                                                jint width, jint height) {
+    setupGraphics(width, height);
+}
+
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_step(JNIEnv *env, jobject obj) {
+    renderFrame();
+}
+
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_tick(JNIEnv *env, jobject obj) {
+    tick();
+}
+
+JNIEXPORT void JNICALL Java_br_odb_nehe_lesson04_GL2JNILib_onDestroy(JNIEnv *env, jobject obj) {
+    shutdown();
+}
+
