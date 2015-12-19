0dde00d350c62873bcb3c397f5c7d1e52fc1a2b1
= <danielmonteiro@id.uff.br>
2015-12-18 23:43:02 -0200

    Forgot to commit the shaders!
    Sorry!

--- lesson06/app/src/main/assets/fragment.glsl
+++ lesson06/app/src/main/assets/fragment.glsl
@@ -1,7 +1,8 @@
 precision mediump float;
-
+varying vec2 vTextureCoords;
 varying vec4 vColour;
+uniform sampler2D sTexture;
 
 void main() {
-    gl_FragColor = vColour;
+    gl_FragColor = texture2D( sTexture, vTextureCoords );
 }
