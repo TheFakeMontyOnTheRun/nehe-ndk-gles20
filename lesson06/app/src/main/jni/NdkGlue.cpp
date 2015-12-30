a127e7b29855ccf1697ee7898a68862b040550b5
= <danielmonteiro@id.uff.br>
2015-12-29 23:23:59 -0200

    Extracting Android IO functionality to external files in Lesson 6
    Since those are very unlikely to change between lessons...

--- lesson06/app/src/main/jni/NdkGlue.cpp
+++ lesson06/app/src/main/jni/NdkGlue.cpp
@@ -32,70 +32,13 @@
 #include "GLES2Lesson.h"
 #include "NdkGlue.h"
 
+#include "android_asset_operations.h"
+
 std::string gVertexShader;
 std::string gFragmentShader;
 GLES2Lesson *gles2Lesson = nullptr;
 
 int *pixels;
-
-
-static int android_read(void *cookie, char *buf, int size) {
-    return AAsset_read((AAsset *) cookie, buf, size);
-}
-
-static int android_write(void *cookie, const char *buf, int size) {
-    return EACCES; // can't provide write access to the apk
-}
-
-static fpos_t android_seek(void *cookie, fpos_t offset, int whence) {
-    return AAsset_seek((AAsset *) cookie, offset, whence);
-}
-
-static int android_close(void *cookie) {
-    AAsset_close((AAsset *) cookie);
-    return 0;
-}
-
-
-FILE *android_fopen(const char *fname, const char *mode, AAssetManager *assetManager) {
-    if (mode[0] == 'w') return NULL;
-
-    AAsset *asset = AAssetManager_open(assetManager, fname, 0);
-    if (!asset) return NULL;
-
-    return funopen(asset, android_read, android_write, android_seek, android_close);
-}
-
-
-std::string readShaderToString(FILE *fileDescriptor) {
-    const unsigned N = 1024;
-    std::string total;
-    while (true) {
-        char buffer[N];
-        size_t read = fread((void *) &buffer[0], 1, N, fileDescriptor);
-        if (read) {
-            for (int c = 0; c < read; ++c) {
-                total.push_back(buffer[c]);
-            }
-        }
-        if (read < N) {
-            break;
-        }
-    }
-
-    return total;
-}
-
-void loadShaders(JNIEnv *env, jobject &obj) {
-    AAssetManager *asset_manager = AAssetManager_fromJava(env, obj);
-    FILE *fd;
-    fd = android_fopen("vertex.glsl", "r", asset_manager);
-    gVertexShader = readShaderToString(fd);
-    fclose(fd);
-    fd = android_fopen("fragment.glsl", "r", asset_manager);
-    gFragmentShader = readShaderToString(fd);
-    fclose(fd);
-}
 
 bool setupGraphics(int w, int h) {
     gles2Lesson = new GLES2Lesson();
