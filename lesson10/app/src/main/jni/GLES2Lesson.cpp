7ba7c0c32df5ad5947a149abf8ab1ce0ac1f85e0
= <danielmonteiro@id.uff.br>
2016-02-23 00:02:31 -0300

    Fix perspective to better match the original Lesson 10
    Otherwise, the perspective would be kinda wonky.

--- lesson10/app/src/main/jni/GLES2Lesson.cpp
+++ lesson10/app/src/main/jni/GLES2Lesson.cpp
@@ -147,8 +147,7 @@ namespace odb {
         glViewport(0, 0, w, h);
         checkGlError("glViewport");
 
-        projectionMatrix = glm::perspective(45.0f, w / h, 0.1f, 1024.0f);
-
+        projectionMatrix = glm::perspective(45.0f, w / h, 0.1f, 100.0f);
 
         glActiveTexture(GL_TEXTURE0);
         textureId = uploadTextureData(textureData, textureWidth, textureHeight);
