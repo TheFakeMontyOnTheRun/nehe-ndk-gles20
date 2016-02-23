b8212dfecd2a81b3951a199d257846ae082151b5
= <danielmonteiro@id.uff.br>
2016-02-22 23:49:56 -0300

    Get rid of unused stuff on fragment shader for Lesson 10
    Fortunately, things that wont be missed.

--- lesson10/app/src/main/assets/fragment.glsl
+++ lesson10/app/src/main/assets/fragment.glsl
@@ -1,14 +1,9 @@
 precision mediump float;
 varying vec2 vTextureCoords;
 uniform sampler2D sTexture;
-uniform vec4 uTint;
 
 void main() {
     vec4 texel = texture2D( sTexture, vTextureCoords );
 
-    if ( texel.a < 0.1 ) {
-        discard;
-    }
-
-    gl_FragColor = texel * uTint;
+    gl_FragColor = texel;
 }
